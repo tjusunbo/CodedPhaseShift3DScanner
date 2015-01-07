14a1d615721aef12e0e06bd0f2570b6eeac74e72
Pranav <pranav8iisc@gmail.com>
2015-01-07 16:54:01 +0530

    created main console and class definition file

--- PhaseShiftSturcturedLight3DScanner.cpp
+++ PhaseShiftSturcturedLight3DScanner.cpp
@@ -1,17 +1,76 @@
+// executes 3D scanning pipeline
+
+
+void readScannerConfiguration()
+{
+nFringePatterns = ;
+nGrayCodedPatternsVertical = ;
+nGrayCodedPatternsHorizontal = ;
+nGrayCodesVertical = ;
+nGrayCodesHorizontal = ;
+fringeWidthVertical = ;
+fringeWidthHorizontal = ;
+triangulationMethod = ;
+projectorImageWidth = ;
+projectorImageHeight = ;
+cameraImageWidth = ;
+cameraImageHeight = ;
+
+projectorGamma = ;
+projectorBrightness = ;
+cameraGamma = ;
+cameraBrightness = ;
+projectorFocus = ;
+cameraFocus = ;
+
+}
+
+
+void systemCalibration()
+{
+}
+
+void generatePatterns()
+{
+}
+
+
+void projectAndCapturePatterns()
+{
+}
+
+void computeWrappedPhase()
+{
+}
+
+
+void computeUnwrappedPhase()
+{
+}
+
+void computeProjectorCameraCorrespondence()
+{
+}
+
+void triangulate()
+{
+}
+
+void savePointCloud()
+{
+}
+
 void scanObject()
 {
+	readScannerConfiguration();
+	systemCalibration();
+	generatePatterns();
+	projectAndCapturePatterns();
+	computeWrappedPhase();
+	computeUnwrappedPhase();
+	computeProjectorCameraCorrespondence();
+	triangulate();
+	savePointCloud();
 
-
-
-
-readScannerConfiguration();
- 
-systemCalibration();
-generatePatterns();
-projectAndCapturePatterns();
-computeWrappedPhase();
-computeUnwrappedPhase();
-computeProjectorCameraCorrespondence();
-        23         3dscanner.triangulate();
-	 24         3dScanner.savePointCloud();
-
+	return;
+}
